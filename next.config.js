cd29b7c81a4db59b1e918115bbfc2353ce2a15e9
Subham <chromaticshubham26@gmail.com>
2025-11-12 21:43:31 +0530

    removed css and js 2

--- next.config.js
+++ next.config.js
@@ -14,10 +14,6 @@ const nextConfig = {
   },
   compress: true,
   poweredByHeader: false,
-  // Experimental features for better performance
-  experimental: {
-    optimizeCss: true,
-  },
   // Remove console logs in production
   webpack: (config, { dev, isServer }) => {
     // SVG loader
